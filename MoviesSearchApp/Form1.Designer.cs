b711ada03019eb13834d91d0304ff76734286d20
Tiisetso Motsie <ST10076861@rcconnect.edu.za>
2025-05-22 12:34:14 +0200

    Add project files.

---
+++ MoviesSearchApp/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace MoviesSearchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lblRating = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.txtMovie);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(102, 53);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(58, 28);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(99, 105);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(30, 13);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Title:";
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(99, 171);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(32, 13);
+            this.lblYear.TabIndex = 3;
+            this.lblYear.Text = "Year:";
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Location = new System.Drawing.Point(99, 230);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(42, 13);
+            this.lblGenre.TabIndex = 4;
+            this.lblGenre.Text = "Genre: ";
+            // 
+            // lblRating
+            // 
+            this.lblRating.AutoSize = true;
+            this.lblRating.Location = new System.Drawing.Point(99, 288);
+            this.lblRating.Name = "lblRating";
+            this.lblRating.Size = new System.Drawing.Size(71, 13);
+            this.lblRating.TabIndex = 5;
+            this.lblRating.Text = "IMDB Rating:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(356, 105);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 235);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblRating);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.Label lblRating;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MoviesSearchApp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace MoviesSearchApp
+{
+    public partial class Form1 : Form
+    {
+        private const string apiKey = "fae137e";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtMovie(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string movieTitle = textBox1.Text.Trim();
+            if (string.IsNullOrEmpty(movieTitle))
+            {
+                MessageBox.Show("Please enter a title movie: ");
+                return;
+            }
+
+            await SearchMovieAsync(movieTitle);
+
+        }
+
+        private async Task SearchMovieAsync(string title)
+        {
+            string apiUrl = $"https://www.omdbapi.com/?t={Uri.EscapeDataString(title)}&apikey={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject movie = JObject.Parse(json);
+
+                    if (movie["Response"].ToString() == "false")
+                    {
+                        MessageBox.Show("Movie is not found");
+                        return;
+                    }
+
+                    lblTitle.Text = "Title: " + (string)movie["Title"];
+                    lblYear.Text = "Year: " + (string)movie["Year"];
+                    lblGenre.Text = "Genre: " + (string)movie["Genre"];
+                    lblRating.Text = "IMDB Rating: " + (string)movie["imdbRating"];
+
+                    string posterUrl = (string)movie["Poster"];
+                    if (!string.IsNullOrEmpty(posterUrl) && posterUrl != "N/A")
+                    {
+
+                        pictureBox1.Load(posterUrl);
+
+                    }
+                    else
+                    {
+                        pictureBox1.Image = null;
+                    }
+
+
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+
+                }
+
+
+            }
+        }
+
+    }
+}
